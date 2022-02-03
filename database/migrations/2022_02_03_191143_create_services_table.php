2ca5d9b8664f6de774c5b5785d95c1de6d6d4c10
Elvis-cpu <ejcvpp@gmail.com>
2022-02-03 17:46:20 -0600

    avances

---
+++ database/migrations/2022_02_03_191143_create_services_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateServicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('name');
+            $table->integer('status')->default(1);
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('update_at')->nullable();
+
+    
+
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('services');
+    }
+}
